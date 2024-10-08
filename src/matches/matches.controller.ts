6940a4907ece567569d044882786a26a1c921e06
Anaisa06 <isa.alzate98@gmail.com>
2024-10-07 19:51:16 -0500

    feat: finish match and add points to the players

--- src/matches/matches.controller.ts
+++ src/matches/matches.controller.ts
@@ -4,6 +4,7 @@ import { CreateMatchDto } from './dto/create-match.dto';
 import { UpdateMatchDto } from './dto/update-match.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { MatchQueryDto } from './dto/match-query.dto';
+import { FinishMatchDto } from './dto/finish-match.dto';
 
 @ApiTags('Matches')
 @Controller('matches')
@@ -18,6 +19,11 @@ export class MatchesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.matchesService.findOne(+id);
+  }
+
+  @Patch('finish/:id')
+  finishMatch(@Param('id') id: string, @Body() finishMatchDto: FinishMatchDto) {
+    return this.matchesService.completeMatch(+id, finishMatchDto)
   }
 
   @Patch(':id')
