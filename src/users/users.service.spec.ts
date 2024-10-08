ec6237bdb24469ff8478db7caf0195ccd89709f5
Anaisa06 <isa.alzate98@gmail.com>
2024-10-07 21:50:22 -0500

    feat: start testing

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Role } from 'src/common/enums/roles.enum';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repository: any;
 
   beforeEach(async () => {
+
+    const mockRepository = {
+      find: jest.fn(),
+      findOne: jest.fn(),
+      findOneBy: jest.fn()
+    }
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository
+        }
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  describe('findAllUsers', () => {
+    it('should return all users', async () => {
+      const repositorySpy = jest.spyOn(repository, 'find')
+      .mockResolvedValue([{ id: 1, name: 'Lulú', email: 'lulu@gmail.com', role: Role.ADMIN, password: '123' }] as unknown as User[])
+
+      expect(service.findAll()).resolves.toStrictEqual([{ id: 1, name: 'Lulú', email: 'lulu@gmail.com', role: Role.ADMIN, password: '123' }]);
+      expect(repositorySpy).toHaveBeenCalledTimes(1);
+    });
+
+
+  })
 });
